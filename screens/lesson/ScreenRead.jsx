e92620464fcfe5d68266a1eed47236d933776b84
Krystal <lyf0921@yahoo.com.hk>
2024-02-18 02:38:09 +0800

    update get video
    update get video

--- screens/lesson/ScreenRead.jsx
+++ screens/lesson/ScreenRead.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect, useState, useRoute } from 'react';
-import { StyleSheet, Image, TouchableOpacity, Text, FlatList, View, Dimensions } from 'react-native';
+import React, { useEffect, useState, useRoute,useRef } from 'react';
+import { StyleSheet, Image, TouchableOpacity, Text, FlatList, View, Dimensions,Button } from 'react-native';
 import ProgressBar from './ProgressBar';
+import { Video, ResizeMode } from 'expo-av';
+import { height, width } from 'deprecated-react-native-prop-types/DeprecatedImagePropType';
+import { videoMapping } from './VideoSource.jsx';
 
 const ScreenRead = ({ route, navigation }) => {
+  const videoRef = useRef(null);
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   const { lessonId } = route.params;
   const { sectionId } = route.params;
   const [read, setRead] = useState([]);
   let lessonRead;
+ 
+
 
   useEffect(() => {
     const fetchSectionData = async () => {
@@ -39,15 +47,19 @@ const ScreenRead = ({ route, navigation }) => {
     fetchSectionData();
   }, [lessonId, sectionId]);
 
+
+  
+
   const onClickNext = (index) => {
     try {
       lessonRead.scrollToIndex({ animated: true, index: index + 1 });
+    
     } catch (e) {
       console.log();
       navigation.goBack();
     }
   };
-
+  
   return (
     <View style={styles.SectionReadBackgound}>
       <FlatList
@@ -59,10 +71,23 @@ const ScreenRead = ({ route, navigation }) => {
         ref={(ref) => {
           lessonRead = ref;
         }}
+      
         renderItem={({ item, index }) => (
           <View style={styles.SectionReadInformation}>
-            <ProgressBar contentLength={5} contentIndex={index} />
-            <Image style={styles.SectionReadImage} source={item.rImage} />
+          <ProgressBar contentLength={5} contentIndex={index}/>
+        
+          <Video
+        ref={videoRef}
+        style={styles.SectionReadImage}
+        playsInSilentLockedModeIOS ={ true }
+        source={videoMapping[item.contentData]}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+        isMuted={true}
+        volume={1.0}
+      />
+
             <Text style={styles.SectionReadText}>English Name: {item.description}</Text>
             <Text style={styles.SectionReadSubText}>Reference: </Text>
             <Text style={styles.SectionReadSubText}>{item.reference}</Text>
@@ -135,9 +160,28 @@ const styles = StyleSheet.create({
   },
   SectionReadImage: {
     height: 300,
-    width: 300,
+    width: 350,
     resizeMode: 'contain'
-  }
+  },
+  SectionReadVideo: {
+    width: Dimensions.get('window').width - 80,
+    height: Dimensions.get('window').width * 0.5625, // 16:9 aspect ratio
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default ScreenRead;
